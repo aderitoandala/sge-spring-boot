800b08d70d885564cec02888131b5e6bfb41ff72
Adérito Andala <aderitoandala@gmail.com>
2026-02-09 15:20:18 +0200

    Mudei o nome de um campo de error para title no dto de erro

--- src/main/java/com/mz/sge/dto/ApiErrorResponse.java
+++ src/main/java/com/mz/sge/dto/ApiErrorResponse.java
@@ -4,15 +4,15 @@ import java.time.LocalDateTime;
 
 public class ApiErrorResponse{
 private int status;
-private String error;
+private String title;
 private String message;
 private String path;
 private LocalDateTime timestamp;
 
 
-public ApiErrorResponse(int status, String error,String message,String path){
+public ApiErrorResponse(int status, String title,String message,String path){
 this.status=status;
-this.error=error;
+this.title=title;
 this.message=message;
 this.path=path;
 this.timestamp=LocalDateTime.now();
@@ -30,8 +30,8 @@ public String getMessage(){
 return message;
 }
 
-public String getError(){
-return error;
+public String getTitle(){
+return title;
 }
 
 
