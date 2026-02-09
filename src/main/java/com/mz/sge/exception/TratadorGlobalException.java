3a0ec37e260a8b58beb76be4c99468d4ff5a7ddc
Adérito Andala <aderitoandala@gmail.com>
2026-02-09 15:14:54 +0200

    Melhorei o meu handler global

--- src/main/java/com/mz/sge/exception/TratadorGlobalException.java
+++ src/main/java/com/mz/sge/exception/TratadorGlobalException.java
@@ -19,51 +19,66 @@ public class TratadorGlobalException{
 
 //Not Found
 @ExceptionHandler(RecursoNaoEncontradoException.class)
-public ResponseEntity<ApiErrorResponse> recursoNaoEncontradoHandler(RecursoNaoEncontradoException e , HttpServletRequest request){
-log.warn("Recurso não encontrado.caminho:{}",request.getRequestURI(),e);
-ApiErrorResponse erro= new ApiErrorResponse(HttpStatus.NOT_FOUND.value(),"Not Found",e.getMessage(),request.getRequestURI());
+public ResponseEntity<ApiErrorResponse> recursoNaoEncontradoHandler(RecursoNaoEncontradoException ex , HttpServletRequest request){
+log.warn("Recurso não encontrado.caminho:{}",request.getRequestURI(),ex);
+
+HttpStatus status = HttpStatus.NOT_FOUND;
 return 
-ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+ResponseEntity.status(status).body(new ApiErrorResponse(status.value(),status.getReasonPhrase(),ex.getMessage(),request.getRequestURI()));
 }
 
 //Internal server error
 @ExceptionHandler(RuntimeException.class)
-public ResponseEntity<ApiErrorResponse>erroInternoHandler(RuntimeException e, HttpServletRequest request){
-log.error("erro interno",e);
+public ResponseEntity<ApiErrorResponse>erroInternoHandler(RuntimeException ex, HttpServletRequest request){
+log.error("erro interno",ex);
 
+HttpStatus status= HttpStatus.INTERNAL_SERVER_ERROR;
 return
-ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),"Internal Server Error","Ocorreu um erro no servidor. Tente mais tarde",request.getRequestURI()));
+ResponseEntity.status(status).body(new ApiErrorResponse(status.value(),status.getReasonPhrase(),"Ocorreu um erro no servidor. Tente mais tarde",request.getRequestURI()));
 }
 
 //Conflict
 @ExceptionHandler(DataIntegrityViolationException.class)
-public ResponseEntity<ApiErrorResponse>violacaoIntegridadeHandler(DataIntegrityViolationException e,HttpServletRequest request){
-log.warn("erro de integridade no caminho:{}",request.getRequestURI(),e);
+public ResponseEntity<ApiErrorResponse> globalDataIntegrityViolationHandler(DataIntegrityViolationException ex,HttpServletRequest request){
+log.warn("erro de integridade no caminho:{}",request.getRequestURI(),ex);
 
-ApiErrorResponse erro=new ApiErrorResponse(HttpStatus.CONFLICT.value(),"Conflict","Violação de integridade dos dados",request.getRequestURI());
+HttpStatus status = HttpStatus.CONFLICT;
 return
-ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+ResponseEntity.status(status).body(new ApiErrorResponse(status.value(),status.getReasonPhrase(),"Violação de integridade dos dados",request.getRequestURI()));
 }
 
 //Bad request - erro de validacao de campos
 @ExceptionHandler(MethodArgumentNotValidException.class)
-public ResponseEntity<ApiValidationErrorResponse> erroValidacaoHandler(MethodArgumentNotValidException e , HttpServletRequest r){
+public ResponseEntity<ApiValidationErrorResponse> erroValidacaoHandler(MethodArgumentNotValidException ex , HttpServletRequest request){
+log.warn("erro de validação no caminho:{}",request.getRequestURI(),ex);
 
-log.warn("erro de validação no caminho:{}",r.getRequestURI(),e);
+HttpStatus status= HttpStatus.BAD_REQUEST;
 Map<String,String> fields=new HashMap<>();
-e.getBindingResult().getFieldErrors().forEach(field -> fields.put(field.getField(),field.getDefaultMessage()));
+ex.getBindingResult().getFieldErrors().forEach(field -> fields.put(field.getField(),field.getDefaultMessage()));
 
 return
-ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiValidationErrorResponse(HttpStatus.BAD_REQUEST.value(),"Bad Request","Houve falha na validação de um ou  mais campos",fields,r.getRequestURI()));
+ResponseEntity.status(status).body(new ApiValidationErrorResponse(status.value(),status.getReasonPhrase(),"Houve falha na validação de um ou  mais campos",fields,request.getRequestURI()));
 }
 
 //Bad request - erro de conversão de tipos
 @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-public ResponseEntity<ApiErrorResponse> erroConversaoTiposHandler(MethodArgumentTypeMismatchException e,HttpServletRequest r){
-log.warn("erro de conversão de tipos.caminho:{}",r.getRequestURI(),e);
-return ResponseEntity.badRequest().body(new ApiErrorResponse(HttpStatus.BAD_REQUEST.value(),"Bad Request","dado inválido para o campo"+" "+e.getName(),r.getRequestURI()));
+public ResponseEntity<ApiErrorResponse> erroConversaoTiposHandler(MethodArgumentTypeMismatchException ex,HttpServletRequest request){
+log.warn("erro de conversão de tipos.caminho:{}",request.getRequestURI(),ex);
+
+HttpStatus status= HttpStatus.BAD_REQUEST;
+return ResponseEntity.status(status).body(new ApiErrorResponse(status.value(),status.getReasonPhrase(),"dado inválido para o campo"+" "+ex.getName(),request.getRequestURI()));
 
 }
+
+@ExceptionHandler(ViolacaoIntegridadeException.class)
+public ResponseEntity<ApiErrorResponse>violacaoIntegridadeHandler(ViolacaoIntegridadeException ex,HttpServletRequest request){
+log.warn("violação de integridade.caminho:{}",request.getRequestURI(),ex);
+
+HttpStatus status=HttpStatus.CONFLICT;
+return ResponseEntity.status(status).body(new ApiErrorResponse(status.value(),status.getReasonPhrase(),ex.getMessage(),request.getRequestURI()));
+}
+
+
 
 
 }
