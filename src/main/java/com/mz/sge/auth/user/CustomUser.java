36e74572c0090ea5392dd577a3a0258609730cb4
Adérito Andala <aderitoandala@gmail.com>
2026-04-04 09:10:44 +0200

    Criei a classe do usuário

---
+++ src/main/java/com/mz/sge/auth/user/CustomUser.java
@@ -0,0 +1,87 @@
+package com.mz.sge.auth.user;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.List;
+import java.util.Collection;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="CUSTOM_USER")
+@NoArgsConstructor
+public class CustomUser implements UserDetails{
+
+@Id
+@GeneratedValue(strategy = GenerationType.UUID)
+private String id;
+
+@Column(nullable=false,unique=true)
+private String username;
+
+@Column(nullable=false)
+private String password;
+
+@Column(nullable=false)
+@Enumerated(EnumType.STRING)
+private UserRole role;
+
+
+public CustomUser(String username,String password,UserRole role){
+this.username=username;
+this.password=password;
+this.role=role;
+}
+
+
+@Override
+public Collection<? extends GrantedAuthority> getAuthorities(){
+if(this.role==UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER"));
+else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+}
+
+@Override
+public String getUsername(){
+return username;
+}
+
+@Override
+public String getPassword(){
+return password;
+}
+
+public UserRole getRole(){
+return role;
+}
+
+
+   @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+}
