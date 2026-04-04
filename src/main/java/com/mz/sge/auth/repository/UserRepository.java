b332c97188ac8af651062c808fecc86351ffd996
Adérito Andala <aderitoandala@gmail.com>
2026-04-04 20:43:26 +0200

    Criei o repositório para buscar o usuário

---
+++ src/main/java/com/mz/sge/auth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.mz.sge.auth.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.mz.sge.auth.user.CustomUser;
+
+public interface UserRepository extends JpaRepository<CustomUser,String>{
+
+Optional<UserDetails> findByUsername(String username);
+
+}
+
+
+
