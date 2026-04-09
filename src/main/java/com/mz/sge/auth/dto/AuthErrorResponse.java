992c9b277b81e6f57f2b47a965770e51ae7c6f5e
Adérito Andala <aderitoandala@gmail.com>
2026-04-09 12:30:47 +0200

    Defini como as excepções na pasta auth serão mostradas ao usuário

---
+++ src/main/java/com/mz/sge/auth/dto/AuthErrorResponse.java
@@ -0,0 +1,19 @@
+package com.mz.sge.auth.dto;
+import java.time.LocalDateTime;
+
+
+public record AuthErrorResponse(
+int status,
+String title,
+String message,
+LocalDateTime timestamp,
+String path
+){
+	public AuthErrorResponse(int status,String title,String message,String path){
+
+		this(status,title,message,LocalDateTime.now(),path);
+	
+	}
+
+
+}
