8f1845f09ea02b4bb4fedf69466f8e8873f05aee
Adérito Andala <aderitoandala@gmail.com>
2026-04-17 20:12:17 +0200

    Criei um service para gerar e validar token

---
+++ src/main/java/com/mz/sge/auth/exception/TokenGenerationException.java
@@ -0,0 +1,13 @@
+package com.mz.sge.auth.exception;
+
+public class TokenGenerationException extends RuntimeException{
+
+public TokenGenerationException(String message){
+super(message);
+}
+
+
+}
+
+
+
