cfd5e0655b8bebab1074188d5d002aa170e8fef9
Adérito Andala <aderitoandala@gmail.com>
2026-04-29 14:26:33 +0200

    Actualiza as entidades adaptando-as ao postgre-m -Nomes em letra minuscula -Uso de sequence no lugar de identity

--- src/main/java/com/mz/sge/entity/AproveitamentoEntity.java
+++ src/main/java/com/mz/sge/entity/AproveitamentoEntity.java
@@ -1,104 +1,99 @@
 package com.mz.sge.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
 import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import java.io.Serializable;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.UniqueConstraint;
+import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access=AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 @Entity
-@Table(name="APROVEITAMENTO",uniqueConstraints={@UniqueConstraint(name="uk_aluno_disciplina_semestre",columnNames={"aluno_id","disciplina_id","semestre"})})
+@Table(name = "aproveitamento", uniqueConstraints = {
+		@UniqueConstraint(name = "uk_aluno_disciplina_semestre", columnNames = { "aluno_id", "disciplina_id",
+				"semestre" }) })
 public class AproveitamentoEntity implements Serializable {
 
-public static final long serialVersionUID=1L;
+	public static final long serialVersionUID = 1L;
 
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
-private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "aproveitamento_seq")
+	@SequenceGenerator(name = "aproveitamento_seq", sequenceName = "aproveitamento_seq", initialValue = 1, allocationSize = 1)
+	private Long id;
 
-@ManyToOne
-@JoinColumn(name="aluno_id",nullable=false)
-private AlunoEntity aluno;
+	@ManyToOne
+	@JoinColumn(name = "aluno_id", nullable = false)
+	private AlunoEntity aluno;
 
-@ManyToOne
-@JoinColumn(name="disciplina_id",nullable=false)
-private DisciplinaEntity disciplina;
+	@ManyToOne
+	@JoinColumn(name = "disciplina_id", nullable = false)
+	private DisciplinaEntity disciplina;
 
-@Column(nullable=false)
-private Integer semestre;
+	@Column(nullable = false)
+	private Integer semestre;
 
-@Column(nullable=false)
-private Double nota1;
+	@Column(nullable = false)
+	private Double nota1;
 
-@Column(nullable=false)
-private Double nota2;
+	@Column(nullable = false)
+	private Double nota2;
 
-public Long getId(){
-return id;
-}
+	public Long getId() {
+		return id;
+	}
 
-public void setId(Long id){
-this.id=id;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public AlunoEntity getAluno(){
-return aluno;
-}
+	public AlunoEntity getAluno() {
+		return aluno;
+	}
 
-public void setAluno(AlunoEntity aluno){
-this.aluno=aluno;
-}
+	public void setAluno(AlunoEntity aluno) {
+		this.aluno = aluno;
+	}
 
-public DisciplinaEntity getDisciplina(){
-return disciplina;
-}
+	public DisciplinaEntity getDisciplina() {
+		return disciplina;
+	}
 
-public void setDisciplina(DisciplinaEntity disciplina){
-this.disciplina=disciplina;
-}
+	public void setDisciplina(DisciplinaEntity disciplina) {
+		this.disciplina = disciplina;
+	}
 
-public Double getNota1(){
-return nota1;
-}
+	public Double getNota1() {
+		return nota1;
+	}
 
-public void setNota1(Double nota1){
-this.nota1=nota1;
-}
+	public void setNota1(Double nota1) {
+		this.nota1 = nota1;
+	}
 
-public Double getNota2(){
-return nota2;
-}
+	public Double getNota2() {
+		return nota2;
+	}
 
-public void setNota2(Double nota2){
-this.nota2=nota2;
-}
+	public void setNota2(Double nota2) {
+		this.nota2 = nota2;
+	}
 
-public Integer getSemestre(){
-return semestre;
-}
+	public Integer getSemestre() {
+		return semestre;
+	}
 
-public void setSemestre(Integer semestre){
-this.semestre=semestre;
-}
-
-
-
-
-
-
-
-
-
+	public void setSemestre(Integer semestre) {
+		this.semestre = semestre;
+	}
 
 }
--- src/main/java/com/mz/sge/entity/DisciplinaEntity.java
+++ src/main/java/com/mz/sge/entity/DisciplinaEntity.java
@@ -1,74 +1,72 @@
 package com.mz.sge.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Column;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import lombok.NoArgsConstructor;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+
 import java.io.Serializable;
 import java.util.List;
 
- 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
- @AllArgsConstructor
- @NoArgsConstructor(access = AccessLevel.PUBLIC)
- @Entity
- @Table(name ="DISCIPLINA")
- public class DisciplinaEntity implements Serializable {
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Entity
+@Table(name = "disciplina")
+public class DisciplinaEntity implements Serializable {
 
- private static final long serialVersionUID=1L;
+	private static final long serialVersionUID = 1L;
 
- @Id
- @GeneratedValue(strategy= GenerationType.IDENTITY)
- private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "disciplina_seq")
+	@SequenceGenerator(name = "disciplina_seq", sequenceName = "disciplina_seq", initialValue = 1, allocationSize = 1)
+	private Long id;
 
- @Column(nullable=false,length=20,unique=true)
- private String nome;
+	@Column(nullable = false, length = 20, unique = true)
+	private String nome;
 
-@Column(length=100)
- private String descricao;
+	@Column(length = 100)
+	private String descricao;
 
-@OneToMany(mappedBy="disciplina")
-private List<AproveitamentoEntity> aproveitamentos;
+	@OneToMany(mappedBy = "disciplina")
+	private List<AproveitamentoEntity> aproveitamentos;
 
- public String getDescricao() {
-      return this.descricao;
- }
+	public String getDescricao() {
+		return this.descricao;
+	}
 
- public void setDescricao(String descricao) {
-      this.descricao=descricao;
- }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
- public Long getId() {
-     return this.id;
- }
+	public Long getId() {
+		return this.id;
+	}
 
-public void setId(Long id){
-this.id=id;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
- public String getNome(){
- return this.nome;
- }
+	public String getNome() {
+		return this.nome;
+	}
 
- public void setNome(String nome){
- this.nome=nome;
- }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
+	public List<AproveitamentoEntity> getAproveitamentos() {
+		return aproveitamentos;
+	}
 
-public List<AproveitamentoEntity> getAproveitamentos(){
-return aproveitamentos;
-}
-
-public void  setAproveitamentos(List<AproveitamentoEntity> aproveitamentos){
-this.aproveitamentos= aproveitamentos;
-}
-
-
-
+	public void setAproveitamentos(List<AproveitamentoEntity> aproveitamentos) {
+		this.aproveitamentos = aproveitamentos;
+	}
 
 }
