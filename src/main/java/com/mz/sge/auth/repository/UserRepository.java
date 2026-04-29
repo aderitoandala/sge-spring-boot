d0597e50908de4713c355ce672309f29136a5e56
Adérito Andala <aderitoandala@gmail.com>
2026-04-29 14:34:56 +0200

    Actualiza o tipo de id no repositorio do usuario

--- src/main/java/com/mz/sge/auth/repository/UserRepository.java
+++ src/main/java/com/mz/sge/auth/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.mz.sge.auth.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import com.mz.sge.auth.user.CustomUser;
 
-public interface UserRepository extends JpaRepository<CustomUser,String>{
+public interface UserRepository extends JpaRepository<CustomUser,UUID>{
 
 	Optional<UserDetails> findByUsername(String username);
 
