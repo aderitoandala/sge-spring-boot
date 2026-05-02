c6f88e8d4ac64571b988a8476efeaf200b1c2c12
Adérito Andala <aderitoandala@gmail.com>
2026-05-02 22:23:05 +0200

    Torna público o endpoint root

--- src/main/java/com/mz/sge/auth/security/SecurityConfig.java
+++ src/main/java/com/mz/sge/auth/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-						.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+						.requestMatchers("/","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 						.anyRequest().authenticated())
 
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
