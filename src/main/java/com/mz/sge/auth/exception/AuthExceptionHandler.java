2f5d13d1bc7f07a1954a770ce260046f19b158b7
Adérito Andala <aderitoandala@gmail.com>
2026-05-11 19:48:24 +0200

    Trata excepcoes de autenticacao
    - BadCredentialsException(credenciais invalidas)
    - TokenGenerationsException(Erro ao gerar o token)
    - Todos resultam em 401 Unauthorized

--- src/main/java/com/mz/sge/auth/exception/AuthExceptionHandler.java
+++ src/main/java/com/mz/sge/auth/exception/AuthExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.mz.sge.auth.dto.AuthErrorResponse;
 
@@ -27,5 +29,34 @@ public class AuthExceptionHandler {
 
 		return ResponseEntity.status(status).body(response);
 	}
+
+@ExceptionHandler(TokenGenerationException.class)
+public ResponseEntity<AuthErrorResponse> tokenGenerationHandler(TokenGenerationException ex,HttpServletRequest request ){
+log.error("error while generate token.path:{}",request.getServletPath(),ex);
+
+HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+AuthErrorResponse response = new AuthErrorResponse(status.value(),status.getReasonPhrase(),"Login failed.Please try again later!",request.getRequestURI());
+return ResponseEntity.status(status).body(response);
+}
+
+
+@ExceptionHandler(UsernameNotFoundException.class)
+public ResponseEntity<AuthErrorResponse> usernameNotFoundHandler(UsernameNotFoundException ex,HttpServletRequest request ){
+log.warn("user not found.path:{}",request.getServletPath(),ex);
+
+HttpStatus status = HttpStatus.UNAUTHORIZED;
+AuthErrorResponse response = new AuthErrorResponse(status.value(),status.getReasonPhrase(),"Invalid credentials",request.getRequestURI());
+return ResponseEntity.status(status).body(response);
+}
+
+@ExceptionHandler(BadCredentialsException.class)
+public ResponseEntity<AuthErrorResponse> badCredentialsHandler(BadCredentialsException ex, HttpServletRequest request) {
+    log.warn("authentication failed.path:{}", request.getServletPath());
+
+    HttpStatus status = HttpStatus.UNAUTHORIZED;
+    AuthErrorResponse response = new AuthErrorResponse(status.value(),status.getReasonPhrase(),"Invalid credentials",request.getRequestURI());
+    return ResponseEntity.status(status).body(response);
+}
+
 
 }
